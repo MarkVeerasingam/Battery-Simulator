f8de5cc9823d6c3c7459631f8d85189ea5342cc2
Mark Veerasingam <markveer70@gmail.com>
2024-07-12 03:30:35 +0100

    made an oop idea in test.ipynb

--- BatterySimulator/main.py
+++ BatterySimulator/main.py
@@ -5,9 +5,5 @@ from Simulation.SingleCell_Experiment import bp_LFP_experiment
 app = Flask(__name__)
 app.register_blueprint(bp_LFP_experiment, url_prefix='/LFP/singleCell')
 
-# Database
-# url = os.getenv("DATABASE_URL")
-# connection = psycopg2.connect(url)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8084)
