2ba01cd31d72769b5f29e0685f86e0a458a98029
MVeerasi <mark.veerasingam@analog.com>
2024-08-06 22:10:15 +0100

    added flask cors to talk to JS fetch api

--- BatterySimulator/main.py
+++ BatterySimulator/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from config.Config import BatteryConfiguration, SolverConfiguration, DriveCycleFile, SimulationConfiguration
 from App.SimulationRunner import SimulationRunner
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/simulate', methods=['POST'])
 def simulate():
