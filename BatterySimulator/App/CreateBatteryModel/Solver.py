4f6f9f232769ea5857d41ae40b14ea1f0ea4cc2a
MVeerasi <mark.veerasingam@analog.com>
2024-08-30 16:00:21 +0100

    casadi solver mode is now configurable in the post api

--- BatterySimulator/App/CreateBatteryModel/Solver.py
+++ BatterySimulator/App/CreateBatteryModel/Solver.py
@@ -10,11 +10,14 @@ class Solver:
         atol = config.tolerance.get("atol", 1e-6)  
         rtol = config.tolerance.get("rtol", 1e-6) 
 
+        # get the config mode for the casadi solver, default mode is "safe"
+        mode = config.mode
+
         # crude check if the solver is CasadiSolver
         if solver_type != "CasadiSolver":
             raise ValueError("Invalid solver, must be CasadiSolver")
         
         # create solver in pybamm. need to make solver it's own class in Config.py to handle more parameterization
-        solver = pybamm.CasadiSolver(mode="safe", atol=atol, rtol=rtol)
+        solver = pybamm.CasadiSolver(mode=mode, atol=atol, rtol=rtol)
         solver._on_extrapolation = "warn"
         return solver
