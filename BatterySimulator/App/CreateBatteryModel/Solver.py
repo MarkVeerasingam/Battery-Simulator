fc464f0244817c50a27e6a1defee1c49d84c5096
mark <mark.veerasingam@analog.com>
2024-09-06 16:25:52 +0100

    updated simulation.py + dockerized with ubuntu

--- BatterySimulator/App/CreateBatteryModel/Solver.py
+++ BatterySimulator/App/CreateBatteryModel/Solver.py
@@ -5,19 +5,18 @@ class Solver:
     @staticmethod
     def create(config: SolverConfiguration):
         solver_type = config.solver
-
-        # Default to 1e-6 if not provided
         atol = config.tolerance.get("atol", 1e-6)  
         rtol = config.tolerance.get("rtol", 1e-6) 
-
-        # get the config mode for the casadi solver, default mode is "safe"
         mode = config.mode
 
-        # crude check if the solver is CasadiSolver
-        if solver_type != "CasadiSolver":
-            raise ValueError("Invalid solver, must be CasadiSolver")
-        
-        # create solver in pybamm. need to make solver it's own class in Config.py to handle more parameterization
-        solver = pybamm.CasadiSolver(mode=mode, atol=atol, rtol=rtol)
+        if solver_type == "CasadiSolver":
+            if mode not in ["safe", "fast", "fast with events"]:
+                raise ValueError(f"Invalid CasadiSolver mode: {mode}. Must be 'safe', 'fast', or 'fast with events'")
+            solver = pybamm.CasadiSolver(mode=mode, atol=atol, rtol=rtol)
+        elif solver_type == "IDAKLUSolver":
+            solver = pybamm.IDAKLUSolver(atol=atol, rtol=rtol)
+        else:
+            raise ValueError("Invalid solver, must be 'CasadiSolver' or 'IDAKLUSolver'")
+
         solver._on_extrapolation = "warn"
         return solver
