d6e788a24d638ffc4b6adf547501bdf321822eec
Mark <markveer70@gmail.com>
2024-09-30 22:07:40 +0100

    updated SimulationRunner.py to include simulation type classes + updated display_results to not call from simulation but ./self

--- BatterySimulator/App/Simulations/SimulationRunner.py
+++ BatterySimulator/App/Simulations/SimulationRunner.py
@@ -1,52 +1,97 @@
-# from config.Config import BatteryConfiguration, SolverConfiguration, SimulationConfiguration, ElectrochemicalConfiguration
-# from App.Simulations.Simulation import Simulation
-# from typing import List
+from App.CreateBatteryModel.ElectrochemicalModel import ElectrochemicalModel
+from App.CreateBatteryModel.ParameterValues import ParameterValues
+from App.CreateBatteryModel.Solver import Solver
+from App.Simulations.ExperimentSimulation import ExperimentSimulation
+from App.Simulations.TimeEvalSimulation import TimeEvalSimulation
+from config.Config import BatteryConfiguration, SolverConfiguration, SimulationConfiguration, ElectrochemicalConfiguration
+from typing import List
 
-# class SimulationRunner:
-#     def __init__(self, battery_config: BatteryConfiguration, solver_config: SolverConfiguration, electrochemical_config: ElectrochemicalConfiguration):
-#         self.simulation = Simulation(battery_config=battery_config, solver_config=solver_config, electrochemical_config=electrochemical_config)
-    
-#     def run_simulation(self, config: SimulationConfiguration):
-#         return self.simulation.execute_simulation(config)
+class SimulationRunner:
+    def __init__(self, battery_config, solver_config, electrochemical_config):
+            """
+            Initialize the SimulationRunner by creating the battery model components.
 
-#     def display_results(self, selected_params: List[str]):
-#         if self.simulation.results is None:
-#             print("No results to display. Run the simulation first.")
-#             return
+            Parameters:
+            - battery_config: The configuration for the battery model (parameters, chemistry).
+            - solver_config: The configuration for the solver (type, tolerance).
+            - electrochemical_config: The configuration for the electrochemical model (thermal, geometry).
+            """
+            # Create the electrochemical model
+            self.electrochemical_model = ElectrochemicalModel.create(electrochemical_config)
 
-#         """
-#         this is the idea of how i want to display the results of parameters based on a rapid protoype form a jupyter notebook.
+            # Create the parameter values (either BPX or built-in pybamm sets)
+            self.parameter_values = ParameterValues.create(battery_config)
+
+            # Create the solver (CasadiSolver, IDAKLUSolver, etc.)
+            self.solver = Solver.create(solver_config)    
+
+    def run_simulation(self, config):
+        """
+        Run the simulation based on the provided configuration.
         
-#         solution = simulation.solve([0,3600]) # not exclusive to only time_eval, can work with any
+        Parameters:
+        - config: The configuration for the simulation (experiment, t_eval, drive cycle).
+        """
+        if config.experiment:
+            experiment_simulation = ExperimentSimulation(
+                electrochemical_model=self.electrochemical_model,
+                parameter_values=self.parameter_values,
+                solver=self.solver
+            )
+            return experiment_simulation.run(experiment=config.experiment)
 
-#         selected_parameters = ['Voltage [V]', 'Current [A]', 'Battery open-circuit voltage [V]', 'Discharge capacity [A.h]']
+        elif config.t_eval:
+            time_eval_simulation = TimeEvalSimulation(
+                electrochemical_model=self.electrochemical_model,
+                parameter_values=self.parameter_values,
+                solver=self.solver
+            )
+            return time_eval_simulation.run(t_eval=config.t_eval)
+        
+        # do drive cycle simulation logic next
+        
+        else:
+            raise ValueError("Invalid simulation type provided.")
 
-#         for param in selected_parameters:
-#                 try:
-#                     # Access the data using the parameter names as keys
-#                     data = solution[param]
-#                     print(f"\nData for {param}:")
-#                     print(data.entries)
-#                 except KeyError:
-#                     print(f"\nParameter '{param}' not found in the simulation results.")
-#         """
+    def display_results(self, selected_params: List[str]):
+        """
+        this is the idea of how i want to display the results of parameters based on a rapid protoype form a jupyter notebook.
+        
+        solution = simulation.solve([0,3600]) # not exclusive to only time_eval, can work with any
 
-#         results = {}
-#         time_s = self.simulation.results["Time [s]"].entries
+        selected_parameters = ['Voltage [V]', 'Current [A]', 'Battery open-circuit voltage [V]', 'Discharge capacity [A.h]']
 
-#         for i, current_time in enumerate(time_s):
-#             time_label = f"{current_time}s"
-#             results[time_label] = {}
-#             for param in selected_params:
-#                 try:
-#                     data = self.simulation.results[param].entries
-#                     results[time_label][param] = data[i]
-#                 except KeyError:
-#                     results[time_label][param] = None
+        for param in selected_parameters:
+                try:
+                    # Access the data using the parameter names as keys
+                    data = solution[param]
+                    print(f"\nData for {param}:")
+                    print(data.entries)
+                except KeyError:
+                    print(f"\nParameter '{param}' not found in the simulation results.")
+        """
 
-#         for time, params in results.items():
-#             print(f"\nTime: {time} s")
-#             for param, value in params.items():
-#                 print(f"{param}: {value}")
+        if self.results is None:
+            print("No results to display. Please run the simulation first.")
+            return
 
-#         return results
+        results = {}
+        time_s = self.results["Time [s]"].entries
+
+        for i, current_time in enumerate(time_s):
+            time_label = f"{current_time}s"
+            results[time_label] = {}
+            for param in selected_params:
+                try:
+                    data = self.results[param].entries
+                    results[time_label][param] = data[i]
+                except KeyError:
+                    results[time_label][param] = None
+                    print(f"Warning: Parameter '{param}' not found in the simulation results.")
+
+        for time, params in results.items():
+            print(f"\nTime: {time} s")
+            for param, value in params.items():
+                print(f"{param}: {value}")
+
+        return results
--- BatterySimulator/App/Simulations/TimeEvalSimulation.py
+++ BatterySimulator/App/Simulations/TimeEvalSimulation.py
@@ -2,10 +2,4 @@ from App.Simulations.BaseSimulation import BaseSimulation
 
 class TimeEvalSimulation(BaseSimulation):
     def run(self, t_eval):
-        """
-        Run a simulation over a specified time period.
-        
-        Parameters:
-        - t_eval: List of time points over which the simulation will be evaluated.
-        """
         return self.run_simulation(t_eval=t_eval)
