6ed321f43384cb1d1965d302153896173cce02cc
Mark <markveer70@gmail.com>
2024-10-01 11:32:07 +0100

    created object files for Model,ParameterValues,Solver and Simulation.py

--- BatterySimulator/config/Config.py
+++ BatterySimulator/config/Config.py
@@ -22,7 +22,7 @@ class ElectrochemicalModelConfiguration(BaseModel):
     # set these as optional, however the default pybamm options of the electrochemical is cell_geometry: 'arbitrary', thermal: 'isothermal'
     # https://docs.pybamm.org/en/stable/source/examples/notebooks/models/using-model-options_thermal-example.html
     thermal_model: Optional[StrictStr] = Field(
-        description="The thermal model option for the battery simulation",
+        description="The thermal model option forq the battery simulation",
         example="isothermal"
     )
     cell_geometry: Optional[StrictStr] = Field(
