0a19e5181839cffb725626d6681c5923c8ad217a
Mark <markveer70@gmail.com>
2024-10-17 00:29:11 +0100

    Merged CircuitModel and CircuitConfiguration

--- BatterySimulator/config/ECM_Config/ECM_Parameters.py
+++ BatterySimulator/config/ECM_Config/ECM_Parameters.py
@@ -1,13 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
-class CircuitModel(BaseModel):
-    is_2RC: bool = Field(
-        default=False,
-        description="If the Thevein Model is a 2RC Model",
-        example=False,
-    )
-
 class CircuitComponent(BaseModel):
     name: str = Field(
         ..., 
@@ -26,6 +19,11 @@ class CircuitComponent(BaseModel):
     )
 
 class CircuitConfiguration(BaseModel):
+    is_2RC: bool = Field(
+        default=False,
+        description="If the Thevenin Model is a 2RC Model",
+        example=False,
+    )
     components: List[CircuitComponent] = Field(
         ..., 
         description="A list of circuit components for the model, each with a name, value, and unit."
